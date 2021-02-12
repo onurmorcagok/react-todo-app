cad885c2c199299a491a97415e1f1d0dca262500
Onur MORCAGÖK <55251990+onurmorcagok@users.noreply.github.com>
2021-02-12 23:17:56 +0300

    TodoForm is updated

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 
 function TodoForm(props) {
+  
   const [input, setInput] = useState(props.edit ? props.edit.value : "");
 
   const inputRef = useRef(null);
