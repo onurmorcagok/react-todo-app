1230226f62b9cf1b8aca5fcc0343cc686447012a
Onur MORCAGÖK <55251990+onurmorcagok@users.noreply.github.com>
2021-02-25 22:44:51 +0300

    Todo is updated, development continued

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -18,7 +18,7 @@ function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
   };
 
   if (edit.id) {
-    return <TodoForm edit={edit} onSubmit={submitUpdate} />;
+    return <TodoForm edit={edit} onSubmit={submitUpdate} />; // Update !
   }
 
   return todos.map((todo, index) => (
